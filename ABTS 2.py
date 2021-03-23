152b4676f172ad53f226724e95788e621b09095d
jukellam <justin@wintermindgroup.com>
2021-03-23 14:09:56 -0600

    Creating Section 2's code.

---
+++ ABTS 2.py
@@ -0,0 +1,18 @@
+#password = 'rosebud'
+#if password == 'swordfish':
+#    print('Access granted')
+#else:
+#    print('Wrong password')
+
+name = 'Bob'
+age = 3000
+if name == 'Alice':
+    print ('Hi Alice')
+elif age < 12:
+    print('You are not Alice, kiddo')
+elif age > 2000:
+    print('Unless you, Alice is not am immortal vampire')
+elif age > 100:
+    print('I dont think so, Grandmother')
+else:
+    print('Hello Alice!')
