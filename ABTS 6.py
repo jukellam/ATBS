adbb4d670c03a51c661c572c856b0728d245f581
jukellam <justin@wintermindgroup.com>
2021-03-30 15:00:38 -0600

    uploading files for Lesson 6

---
+++ ABTS 6.py
@@ -0,0 +1,9 @@
+print("Not much to upload this lesson so I will just post a link to a great song for you")
+print("https://open.spotify.com/track/0RliZNSYxNPfwHI6fyD7Ts?si=n8K8x-O-TyuhRhamrAGLuA")
+
+def eggs(Parameter):
+    Parameter.append('Hello')
+
+spam = [1, 2, 3]
+eggs(spam)
+print(spam)
