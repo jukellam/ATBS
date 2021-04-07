7ddae9b125303ab22d8d6a4af9dd278bc402b72b
jukellam <justin@wintermindgroup.com>
2021-04-07 13:36:51 -0600

    file for section 7

---
+++ ABTS 7.py
@@ -0,0 +1,11 @@
+import pprint
+
+message = 'It was a bright cold day in April, and the clocks were striking thirteen.'
+count = {} 
+
+for character in message.lower():
+    count.setdefault(character, 0) #If you don't find a letter in the key, set the value of that letter to 0
+    count[character] = count[character] + 1
+
+text = pprint.pformat(count)
+print(text)
